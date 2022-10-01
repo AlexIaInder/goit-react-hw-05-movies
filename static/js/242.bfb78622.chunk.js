37cde519934f347d99c6a85a0605ac97ffcd2c0c
AlexIaInder <103319919+AlexIaInder@users.noreply.github.com>
2022-10-01 21:50:00 +0000

    Deploying to gh-pages from @ AlexIaInder/goit-react-hw-05-movies@da643c1132894e86098f2d549e180daadc811f17 🚀

---
+++ static/js/242.bfb78622.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[242],{3242:function(e,t,n){n.r(t);var r=n(6731),o=n(184);t.default=function(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("h2",{children:"Oops... nothing found..."}),(0,o.jsx)(r.rU,{to:"/",children:"Back to home"})]})}}}]);
+//# sourceMappingURL=242.bfb78622.chunk.js.map
